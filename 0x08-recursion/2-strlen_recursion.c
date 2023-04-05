1fd6659a129fbfccdd702a96ce64aa30bfde1bc6
Kawtar El khoumani <kawtar.elkhoumani@outlook.com>
2023-04-05 13:44:42 -0700

    recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,7 +2,8 @@
 
 /**
  * _strlen_recursion - returns the length of a string
- * @s: string to calculate the length of 
+ * @s: string to calculate the length of
+ *
  * Return: length of the string
  */
 int _strlen_recursion(char *s)
