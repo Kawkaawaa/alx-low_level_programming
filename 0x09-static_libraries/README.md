2abafbfa1ef88c9453a1c08236adf884aed44132
Kawtar El khoumani <kawtar.elkhoumani@outlook.com>
2023-04-08 15:29:22 -0700

    liball.c

--- 0x09-static_libraries/README.md
+++ 0x09-static_libraries/README.md
@@ -1,1 +1,2 @@
 limby.a conatins a function that creates the static library limby.a containgall the functions that we learned in the previous lesson
+create_static_lib.shis a script that creates a static library called liball.a from all the c files that are in the current directory
---
+++ 0x09-static_libraries/create_static_lib.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#create a create_static_lib.sh 
+#create an object file for each .c file in the current directory
+C_FILES=$(find . -maxdepth 1 -type f -name "*.c")
+#create a static library from all the object files
+ar rcs liball.a $(for file in $C_FILES; do gcc -c $file;echo ${file%.c}.o; done)
