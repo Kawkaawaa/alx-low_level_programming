56581733e64ef0b73e53792956a2e29d3d010fa8
Kawtar El khoumani <kawtar.elkhoumani@outlook.com>
2023-04-08 15:36:05 -0700

    liball.a

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -3,4 +3,4 @@
 #create an object file for each .c file in the current directory
 C_FILES=$(find . -maxdepth 1 -type f -name "*.c")
 #create a static library from all the object files
-ar rcs liball.a $(for file in $C_FILES; do gcc -c $file;echo ${file%.c}.o; done)
+ar rcs liball.a $(for file in $C_FILES; do gcc -c $file; echo ${file%.c}.o; done)
