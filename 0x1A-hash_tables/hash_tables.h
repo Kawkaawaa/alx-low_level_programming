d5fbfcf94b09823b74a7e103406ed4029958c08c
kawtar Elkhoumani <kawtar.elkhoumani@outlook.com>
2023-12-01 04:24:49 -0800

    #

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -8,12 +8,12 @@
 /**
  * struct hash_node_s - Node of a hash table
  *
- * @key: The key string
- * The key is unique in the Hashtable
+ * @key: The key, string
+ * The key is unique in the HashTable
  * @value: The value corresponding to a key
  * @next: A pointer to the next node of the List
  */
-type struct hash_node_s
+typedef struct hash_node_s
 {
 	char *key;
 	char *value;
